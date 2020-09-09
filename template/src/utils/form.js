02f67dbcd9922fdc2fe49a19730ea83889fc3ed0
Patrick Santos <patrickspteixeira@gmail.com>
2020-09-09 18:31:38 -0300

    chore: add final-form error parser for adonis

--- template/src/utils/form.js
+++ template/src/utils/form.js
@@ -19,3 +19,31 @@ export const format = mask => {
     parse: fieldValue => fieldValue.replace(/[^\d]/g, ''),
   };
 };
+
+/**
+ * Example Usage (Final Form 'onSubmit' prop):
+ *
+ * const onSubmit = async values => {
+ *   try {
+ *     await api.post('endpoint', values);
+ *   } catch (error) {
+ *     return errorValidatorBack(error);
+ *   }
+ * }
+ * @param {String} mask
+ */
+export const errorValidatorBack = error => {
+  if (
+    error?.response?.status === 422 &&
+    error?.response?.data?.error &&
+    Array.isArray(error.response.data.error)
+  ) {
+    const formErrors = {};
+
+    error.response.data.error.forEach(item => {
+      formErrors[item.field] = item.message;
+    });
+
+    return formErrors;
+  }
+};
