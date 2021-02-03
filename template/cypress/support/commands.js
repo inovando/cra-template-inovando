86cf616fac86c714481f3cdca975cb22d8321b28
Patrick Santos <patrickspteixeira@gmail.com>
2021-02-03 13:54:24 -0300

    feat(cypress): add file upload support

--- template/cypress/support/commands.js
+++ template/cypress/support/commands.js
@@ -23,6 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+import 'cypress-file-upload';
 
 Cypress.Commands.add('login', () => {
   cy.visit(Cypress.env('HOST'));
